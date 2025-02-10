4e0b578245e6e29a639ecfb0b8c1dd7ec39c067e
krzysztofee <gitaragra@gmail.com>
2025-02-10 21:48:24 +0100

    first commit

---
+++ app/posts/page.tsx
@@ -0,0 +1,7 @@
+export default function PostsMain() {
+  return (
+    <main className="text-center pt-32 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">Witamy</h1>
+    </main>
+  );
+}
