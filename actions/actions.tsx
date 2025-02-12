641dea0722dd9c5658c46e75fa66baa4dc9f3e1d
krzysztofee <gitaragra@gmail.com>
2025-02-12 15:22:58 +0100

    Add   revalidatePath("/posts"); to create post

--- actions/actions.tsx
+++ actions/actions.tsx
@@ -1,12 +1,19 @@
 "use server";
 
-import {prisma} from "@/lib/db";
+import { prisma } from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const createPost = async (formData: FormData) => {
+  const { isAuthenticated } = getKindeServerSession();
+
+  if (!(await isAuthenticated())) {
+    redirect("/api/auth/login");
+  }
+
   const title = formData.get("title") as string;
   const body = formData.get("body") as string;
-
 
   await prisma.post.create({
     data: {
--- app/create-post/page.tsx
+++ app/create-post/page.tsx
@@ -1,4 +1,5 @@
 import { createPost } from "@/actions/actions";
+import Form from "@/components/form";
 import {
   getKindeServerSession,
   LogoutLink,
@@ -14,23 +15,9 @@ const PageCreatePost = async () => {
   return (
     <main className="text-center pt-32 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">Zapisz Post</h1>
-      <form action={createPost} className="flex flex-col gap-4 max-w-[450px] mx-auto">
-        <input
-          type="text"
-          name="title"
-          placeholder="Tytuł"
-          className="border rounted px-3"
-        />
-        <textarea
-          name="body"
-          placeholder="Opis"
-          rows={6}
-          className="border rounted px-3"
-        />
-        <button className="text-white bg-blue-500 px-2 py-1 rounded">
-          Zapisz
-        </button>
-      </form>
+
+      <Form />
+
       <LogoutLink className="border border-zinc-800 px-2 py-1 mx-auto size-fit rounded block mt-6">
         Wyloguj
       </LogoutLink>
